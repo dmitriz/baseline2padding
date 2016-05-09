42392d0ddcff423e56d81faa04c6d748824d7f0c
dmitriz <dmitri14@gmail.com>
2016-05-09 05:56:36 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+module.export = baseline2padding
+
+var baseline2padding = { top: top, bottom: bottom }
+
+function top (argument) {
+	// body...
+}
+
+function bottom (argument) {
+	// body...
+}
